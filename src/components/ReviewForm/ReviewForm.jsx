739034c4f263078f46bbb17b0f943f40c67faa64
Jane <tanhuiqing95@hotmail.com>
2024-05-15 18:23:46 +0800

    feat: add review form html

---
+++ src/components/ReviewForm/ReviewForm.jsx
@@ -0,0 +1,59 @@
+function ReviewForm() {
+  return (
+    <>
+      <div>
+        <h1>Review Form</h1>
+        <form>
+          <div>
+            <label htmlFor="time-of-visit">Time Of Visit: </label>
+
+            <select name="time" id="time-of-visit">
+              <option value="morning">Morning</option>
+              <option value="afternoon">Afternoon</option>
+              <option value="evening">Evening</option>
+              <option value="night">Night</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="rating">Rating: </label>
+            <select name="rating" id="rating">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+            </div>
+            <div>
+            <label htmlFor="looks-new">Looks New?: </label>
+            <select name="looks-new" id="looks-new">
+              <option value="yes">Yes</option>
+              <option value="no">No</option>
+            </select>
+            </div>
+            <div>
+            <label htmlFor="pros">Pros: </label>
+              <textarea
+                name="pros"
+                id="pros"
+                placeholder="What do you like about this property?"
+              ></textarea>
+            </div>
+            <div>
+              <label htmlFor="cons">Cons: </label>
+              <textarea
+                name="cons"
+                id="cons"
+                placeholder="What can be improved?"
+              ></textarea>
+            </div>
+            <button type="submit" value="submit">
+              Submit
+            </button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default ReviewForm
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -4,6 +4,7 @@ import NavBar from "../../components/NavBar/NavBar";
 import { LandingPage } from "../LandingPage";
 import Register from "../AuthPages/Register";
 import Login from "../AuthPages/Login";
+import ReviewForm from "../../components/ReviewForm/ReviewForm";
 
 // const log = debug("mern:pages:App:App");
 
@@ -16,6 +17,8 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/addReview" element={<ReviewForm />} />
+
           {/* <Route path="/property/:id" element={<PropertyPage />} /> */}
         </Routes>
       </main>
