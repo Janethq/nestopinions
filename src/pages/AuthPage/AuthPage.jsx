11f8fa5888de5f2086650e84e2ccccc939a98174
Jane <tanhuiqing95@hotmail.com>
2024-05-15 10:04:30 +0800

    feat: install tailwind

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,9 +1,11 @@
 import LoginForm from "../../components/LoginForm/LoginForm";
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import "tailwindcss/tailwind.css";
 
 export default function AuthPage({ setUser }) {
   return (
     <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <LoginForm setUser={setUser} />
       <SignUpForm setUser={setUser} />
     </>
