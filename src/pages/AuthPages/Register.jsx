5463d58fb131c1aa0210f3d65e58c6dfd85c719f
Vanitaa Rai <vanitrai@gmail.com>
2024-05-16 09:51:10 +0800

    fix reorganize logo to assets folder

--- src/pages/AuthPages/Register.jsx
+++ src/pages/AuthPages/Register.jsx
@@ -27,7 +27,7 @@ export default function Register() {
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
             className="mx-auto h-10 w-auto"
-            src="public/logo/icons8-real-estate-96.png"
+            src="assets/logo/icons8-real-estate-96.png"
             alt="NestOpinion Logo"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
