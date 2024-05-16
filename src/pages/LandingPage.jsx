28943b6a6c8154e417888a6b3515f7178f92f78a
Merylcheng <163097111+Merylcheng@users.noreply.github.com>
2024-05-16 23:32:54 +0800

    feat: SearchBar and dropdown menu

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,5 +1,10 @@
-// import {useState} from "react"
+import SearchBar from "../components/Landing/SearchBar";
 
 export const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <div>
+      <h1>Landing Page</h1>
+      <SearchBar />
+    </div>
+  );
 };
