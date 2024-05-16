abe3e72639d40240a18573fd8661678b8293f6ef
Vanitaa Rai <vanitrai@gmail.com>
2024-05-16 23:30:57 +0800

    setup and test login endpoint

--- controllers/authController.js
+++ controllers/authController.js
@@ -34,7 +34,12 @@ const registerUser = async (req, res) => {
   }
 };
 
+const loginUser = (req, res) => {
+  res.json({ msg: "login endpoint" }); //test bruno
+};
+
 module.exports = {
   test,
   registerUser,
+  loginUser,
 };
--- routes/api/authRoutes.js
+++ routes/api/authRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { test, registerUser } = require("../../controllers/authController");
+const {
+  test,
+  registerUser,
+  loginUser,
+} = require("../../controllers/authController");
 
 // /api/users
 router.get("/", test); //bruno --> testing
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 module.exports = router;
