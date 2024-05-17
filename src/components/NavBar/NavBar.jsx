8269859f25967a580e1603586d44e2e201efbbd7
Vanitaa Rai <vanitrai@gmail.com>
2024-05-17 22:25:24 +0800

    feat logic for navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,25 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 export default function NavBar() {
+  const { setAuthUser, authUser } = useContext(AuthContext);
+
+  const handleLogout = () => {
+    setAuthUser(null);
+    localStorage.removeItem("token");
+  };
+
+  if (authUser) {
+    return (
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <span>{authUser.username}</span>
+        <button onClick={handleLogout}>Sign Out</button>
+      </nav>
+    );
+  }
+
   return (
     <nav>
       <NavLink to="/">Home</NavLink>
