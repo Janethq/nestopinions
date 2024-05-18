2149e47a5a9a3c1e53f28e320d092e90b47f8197
Jane <tanhuiqing95@hotmail.com>
2024-05-18 14:57:21 +0800

    display reviews

--- src/components/ReviewDetails/ReviewDetails.jsx
+++ src/components/ReviewDetails/ReviewDetails.jsx
@@ -6,7 +6,7 @@ export default function ReviewDetails() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`/api/addreview`);
+        const response = await fetch(`/api/reviews`);
         const data = await response.json();
         console.log(data);
         setReviewData(data);
@@ -17,44 +17,47 @@ export default function ReviewDetails() {
     fetchData();
   }, []);
 
+  const displayReviews = () => {
+    if (reviewData.length > 0) {
+      return reviewData.map((review, index) => (
+        <div key={index}>
+          <div>
+            <label>Time Of Visit: </label>
+            <span>{review.time}</span>
+          </div>
+          <div>
+            <label>Rating: </label>
+            <span>{review.rating}</span>
+          </div>
+          <div>
+            <label>Looks New?: </label>
+            <span>{review.looksNew}</span>
+          </div>
+          <div>
+            <label>Pros: </label>
+            <span>{review.pros}</span>
+          </div>
+          <div>
+            <label>Cons: </label>
+            <span>{review.cons}</span>
+          </div>
+          <div>
+            <button>Remove Review</button>
+          </div>
+          <div>
+            <button>Edit Review</button>
+          </div>
+        </div>
+      ));
+    } else {
+      return <div>No reviews posted yet.</div>;
+    }
+  };
+
   return (
     <>
-      {reviewData.length > 0 && (
-        <div>
-          <h1>Review Details</h1>
-          {reviewData.map((review, index) => (
-            // set unique key to remove error
-            <div key={index}>
-              <div>
-                <label>Time Of Visit: </label>
-                <span>{review.time}</span>
-              </div>
-              <div>
-                <label>Rating: </label>
-                <span>{review.rating}</span>
-              </div>
-              <div>
-                <label>Looks New?: </label>
-                <span>{review.looksNew}</span>
-              </div>
-              <div>
-                <label>Pros: </label>
-                <span>{review.pros}</span>
-              </div>
-              <div>
-                <label>Cons: </label>
-                <span>{review.cons}</span>
-              </div>
-              <div>
-                <button>Remove Review</button>
-              </div>
-              <div>
-                <button>Edit Review</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      )}
+      <h1>Review Details</h1>
+      {displayReviews()}
     </>
   );
 }
