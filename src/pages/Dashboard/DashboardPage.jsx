b14160d7e840af6091da0796587ab939d058a99c
Vanitaa Rai <vanitrai@gmail.com>
2024-05-19 13:48:10 +0800

    feat functional tabs click

--- src/pages/Dashboard/DashboardPage.jsx
+++ src/pages/Dashboard/DashboardPage.jsx
@@ -1,9 +1,24 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import MyProfileTab from "./MyProfile/MyProfileTab";
+import MyReviewsTab from "./MyReviews/MyReviewsTab";
 
 const DashboardPage = () => {
   const { authUser } = useContext(AuthContext);
+  // eslint-disable-next-line no-unused-vars
+  const [activeTab, setActiveTab] = useState("profileTab"); // or 'reviewsTab'
+
+  const handleTabSelection = (tab) => {
+    setActiveTab(tab);
+  };
+
+  const getTabClassName = (tab) => {
+    return `mt-5 cursor-pointer border-l-2 px-2 py-2 font-semibold transition ${
+      activeTab === tab
+        ? "border-l-indigo-600 text-indigo-600"
+        : "border-transparent text-gray-600"
+    } hover:border-l-indigo-800 hover:text-indigo-800`;
+  };
 
   return (
     <div className="mx-4 min-h-screen max-w-screen-xl sm:mx-8 xl:mx-auto">
@@ -39,33 +54,47 @@ const DashboardPage = () => {
             />
           </svg>
           <ul className="max-h-0 select-none flex-col overflow-hidden rounded-b-lg shadow-md transition-all duration-300 peer-checked:max-h-56 peer-checked:py-3">
-            <li className="cursor-pointer px-3 py-2 text-sm text-slate-600 hover:bg-red-300 hover:text-gray">
+            <li
+              className="cursor-pointer px-3 py-2 text-sm text-slate-600 hover:bg-red-300 hover:text-gray"
+              onClick={() => handleTabSelection("reviewsTab")}
+            >
               My Reviews
+            </li>
+            <li
+              className="cursor-pointer px-3 py-2 text-sm text-slate-600 hover:bg-red-300 hover:text-gray"
+              onClick={() => handleTabSelection("profileTab")}
+            >
+              My Profile
             </li>
           </ul>
         </div>
 
         <div className="col-span-2 hidden sm:block">
           <ul>
-            <li className="mt-5 cursor-pointer border-l-2 border-transparent px-2 py-2 font-semibold text-indigo-600 transition hover:border-l-indigo-800 hover:text-indigo-800">
+            <li
+              className={getTabClassName("reviewsTab")}
+              onClick={() => handleTabSelection("reviewsTab")}
+            >
               My Reviews
             </li>
-            <li className="mt-5 cursor-pointer border-l-2 border-l-indigo-600 px-2 py-2 font-semibold text-indigo-600 transition hover:border-l-indigo-800 hover:text-indigo-800">
+            <li
+              className={getTabClassName("profileTab")}
+              onClick={() => handleTabSelection("profileTab")}
+            >
               My Profile
             </li>
           </ul>
         </div>
 
         <div className="col-span-8 overflow-hidden rounded-xl sm:bg-gray-50 sm:px-8 sm:shadow">
-          <div className="pt-4">
-            <h1 className="py-2 text-2xl font-semibold text-gray-900">
-              Profile Settings
-            </h1>
-          </div>
-          <MyProfileTab email={authUser.email} username={authUser.username} />
+          {activeTab === "profileTab" && (
+            <MyProfileTab email={authUser.email} username={authUser.username} />
+          )}
+          {activeTab === "reviewsTab" && <MyReviewsTab />}
         </div>
       </div>
     </div>
   );
 };
+
 export default DashboardPage;
--- src/pages/Dashboard/MyProfile/MyProfileTab.jsx
+++ src/pages/Dashboard/MyProfile/MyProfileTab.jsx
@@ -5,6 +5,12 @@ import DeleteAcctSection from "./DeleteAcctSection";
 const MyProfileTab = ({ email, username }) => {
   return (
     <div>
+      <div className="pt-4">
+        <h1 className="py-2 text-2xl font-semibold text-gray-900">
+          Profile Settings
+        </h1>
+      </div>
+      <hr className="mt-4 mb-8" />
       <AcctInfoSection email={email} username={username} />
       <hr className="mt-4 mb-8" />
       <UpdatePwSection />
