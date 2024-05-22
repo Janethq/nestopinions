2ea52f9c7cb0b3ceb0fd685a7b58bd2d56c60882
Merylcheng <163097111+Merylcheng@users.noreply.github.com>
2024-05-22 16:08:17 +0800

    style: propertydetails's property

--- controllers/api/propertyController.js
+++ controllers/api/propertyController.js
@@ -30,7 +30,7 @@ const seedData = [
       "https://ohmyhome.com/wp-content/uploads/2021/08/IMG_8871-2-scaled.jpg",
     reviews: [
       {
-        time: "Morning",
+        time: "Afternoon",
         rating: 4,
         looksNew: true,
         pros: "Good Location",
@@ -48,60 +48,66 @@ const seedData = [
       "https://i0.wp.com/lifefinance.com.sg/wp-content/uploads/2020/01/nguyen-thu-hoai-a15b7LYrfbk-unsplash-scaled.jpg?resize=768%2C472&ssl=1",
     reviews: [],
   },
-  // {
-  //   address: "101 Clementi Avenue 3",
-  //   postalCode: 129905,
-  //   area: "Clementi",
-  //   distanceMrt: "150m",
-  //   hdbType: "3-room",
-  //   imageUrl:
-  //     "https://www.mynicehome.gov.sg/wp-content/uploads/Things-to-Know-When-Applying-for-SBF-1.jpg",
-  // },
-  // {
-  //   address: "202 Bedok North Street 1",
-  //   postalCode: 460202,
-  //   area: "Bedok",
-  //   distanceMrt: "600m",
-  //   hdbType: "4-room",
-  //   imageUrl:
-  //     "https://i0.wp.com/lifefinance.com.sg/wp-content/uploads/2022/11/P-Yishun-Boardwalk.jpg?w=1536&ssl=1",
-  // },
-  // {
-  //   address: "201 Tampines st 21",
-  //   postalCode: 520201,
-  //   area: "Tampines",
-  //   distanceMrt: "800m",
-  //   hdbType: "5-room",
-  //   imageUrl:
-  //     "https://ohmyhome.com/wp-content/uploads/2021/08/IMG_8942-2-scaled.jpg",
-  // },
-  // {
-  //   address: "894 Tampines Street 81",
-  //   postalCode: 520894,
-  //   area: "Tampines",
-  //   distanceMrt: "250m",
-  //   hdbType: "3-room",
-  //   imageUrl:
-  //     "https://ohmyhome.com/wp-content/uploads/2021/08/IMG_9074-2-scaled.jpg",
-  // },
-  // {
-  //   address: "374 Tampines Street 34",
-  //   postalCode: 520374,
-  //   area: "Tampines",
-  //   distanceMrt: "400m",
-  //   hdbType: "5-room",
-  //   imageUrl:
-  //     "https://www.mynicehome.gov.sg/wp-content/uploads/A-Singles-Guide-to-Buying-an-HDB-Flat-1.jpg",
-  // },
-  // {
-  //   address: "128D Punggol Field Walk",
-  //   postalCode: 824128,
-  //   area: "Punggol",
-  //   distanceMrt: "300m",
-  //   hdbType: "4-room",
-  //   imageUrl:
-  //     "https://www.mynicehome.gov.sg/wp-content/uploads/A-Singles-Guide-to-Buying-an-HDB-Flat-Cover-1024x576.jpg",
-  // },
+  {
+    address: "101 Clementi Avenue 3",
+    postalCode: 129905,
+    area: "Clementi",
+    distanceMrt: "150m",
+    hdbType: "3-room",
+    imageUrl:
+      "https://www.mynicehome.gov.sg/wp-content/uploads/Things-to-Know-When-Applying-for-SBF-1.jpg",
+    reviews: [],
+  },
+  {
+    address: "202 Bedok North Street 1",
+    postalCode: 460202,
+    area: "Bedok",
+    distanceMrt: "600m",
+    hdbType: "4-room",
+    imageUrl:
+      "https://i0.wp.com/lifefinance.com.sg/wp-content/uploads/2022/11/P-Yishun-Boardwalk.jpg?w=1536&ssl=1",
+    reviews: [],
+  },
+  {
+    address: "201 Tampines st 21",
+    postalCode: 520201,
+    area: "Tampines",
+    distanceMrt: "800m",
+    hdbType: "5-room",
+    imageUrl:
+      "https://ohmyhome.com/wp-content/uploads/2021/08/IMG_8942-2-scaled.jpg",
+    reviews: [],
+  },
+  {
+    address: "894 Tampines Street 81",
+    postalCode: 520894,
+    area: "Tampines",
+    distanceMrt: "250m",
+    hdbType: "3-room",
+    imageUrl:
+      "https://ohmyhome.com/wp-content/uploads/2021/08/IMG_9074-2-scaled.jpg",
+    reviews: [],
+  },
+  {
+    address: "374 Tampines Street 34",
+    postalCode: 520374,
+    area: "Tampines",
+    distanceMrt: "400m",
+    hdbType: "5-room",
+    imageUrl:
+      "https://www.mynicehome.gov.sg/wp-content/uploads/A-Singles-Guide-to-Buying-an-HDB-Flat-1.jpg",
+    reviews: [],
+  },
+  {
+    address: "128D Punggol Field Walk",
+    postalCode: 824128,
+    area: "Punggol",
+    distanceMrt: "300m",
+    hdbType: "4-room",
+    imageUrl:
+      "https://www.mynicehome.gov.sg/wp-content/uploads/A-Singles-Guide-to-Buying-an-HDB-Flat-Cover-1024x576.jpg",
+    reviews: [],
+  },
 ];
 
 // Seed properties
