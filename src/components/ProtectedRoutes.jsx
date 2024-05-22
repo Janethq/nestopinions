57c7c29410dcc9aae78d4212a3fe2d805235b4ad
Vanitaa Rai <vanitrai@gmail.com>
2024-05-22 11:28:18 +0800

    fixing bug

--- src/components/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes.jsx
@@ -1,27 +1,32 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useRef } from "react";
 import { Navigate, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 import toast from "react-hot-toast";
 import { isTokenExpired, removeToken } from "../utils/services/clientToken";
+import debug from "debug";
+
+const log = debug("mern:ProtectedRoutes");
 
 const ProtectedRoute = ({ children }) => {
   const { authUser, setAuthUser } = useContext(AuthContext);
   const location = useLocation();
   const navigate = useNavigate();
-
-  // authUser === null //handleTokenExpiration -->autologout & notify to login/register
+  const hasNotified = useRef(false); //adding flag to prevent double toast notification
 
   useEffect(() => {
     const checkTokenExpiration = () => {
       if (isTokenExpired()) {
-        //notify user
-        toast("Session expired. Please log in again.", {
-          icon: "⚠️",
-        });
+        if (!hasNotified.current) {
+          //notify user
+          toast("Session expired. Please log in again.", {
+            icon: "⚠️",
+          });
+          hasNotified.current = true;
+        }
         //autologout
         setAuthUser(null);
         removeToken();
-        navigate("/login", { state: { from: location } });
+        navigate("/login");
       }
     };
 
@@ -29,10 +34,10 @@ const ProtectedRoute = ({ children }) => {
     checkTokenExpiration();
 
     const interval = setInterval(checkTokenExpiration, 5000);
-
+    log(interval);
     //clean up
     return () => clearInterval(interval);
-  }, [navigate, setAuthUser, location]);
+  }, [navigate, setAuthUser]);
 
   if (!authUser) {
     toast("Login or Register", {
